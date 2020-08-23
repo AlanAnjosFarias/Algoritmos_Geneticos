1d955dff108cf6a680bd3f21bda651c00915092d
Alan dos Anjos Farias <farias_alan@outlook.com>
2020-08-23 12:31:42 -0300

    Update TodosUm.java

--- AlgoritmoGenetico/src/modulo_01/TodosUm.java
+++ AlgoritmoGenetico/src/modulo_01/TodosUm.java
@@ -4,7 +4,7 @@ public class TodosUm {
 
 	public static void main(String[] args) {
 		// Criar objeto do algoritmo genetico
-		AlgoritmoGenetico ag = new AlgoritmoGenetico(100, 0.001, 0.95, 2);
+		AlgoritmoGenetico ag = new AlgoritmoGenetico(100, 0.001, 0.95, 5);
 
 		// criar a populacao do algoritomo
 		Populacao populacao = ag.iniciarPopulacao(180);
